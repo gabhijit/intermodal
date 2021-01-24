3ef70aa08da97327386b79bbd322bdee73743c44
Abhijit Gadgil <gabhijit@iitbombay.org>
2021-01-24 19:19:29 +0530

    Added a few more tests

--- src/image/docker/tests.rs
+++ src/image/docker/tests.rs
@@ -60,3 +60,33 @@ async fn test_get_manifest_list_success() {
 
     assert!(manifest.is_ok(), "ref: {:?}, {:?}", ref_str, manifest);
 }
+
+#[tokio::test]
+async fn test_get_resolved_manifest() {
+    init();
+
+    let mock_server = wiremock::MockServer::start().await;
+    let image_name = format!("docker://{}/library/fedora", mock_server.address());
+
+    let mock_response = ResponseTemplate::new(200).set_body_raw(
+        DOCKER_LIST_MANIFEST_BLOB.as_bytes().to_owned(),
+        MEDIA_TYPE_DOCKER_V2_LIST,
+    );
+
+    let mock_manifest = Mock::given(method("GET")).respond_with(mock_response);
+
+    mock_server.register(mock_manifest).await;
+
+    let mock_ref = create_mock_reference(&image_name);
+    assert!(mock_ref.is_ok(), "{:?}", mock_ref);
+
+    let mock_ref = mock_ref.unwrap();
+
+    let ref_str = mock_ref.string_within_transport();
+    let image = mock_ref.new_image();
+    assert!(image.is_ok());
+
+    let manifest = image.unwrap().manifest().await;
+
+    assert!(manifest.is_ok(), "ref: {:?}, {:?}", ref_str, manifest);
+}
