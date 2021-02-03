4067e4e58ca6cbbc370c4e3831b3982a25fe5b51
Abhijit Gadgil <gabhijit@iitbombay.org>
2021-02-03 14:08:16 +0530

    Supporting image tags in `OCIImageLayout` and more
    We are now accepting, optional image tag while creating a `new`
    `OCIImageLayout` structure. The resultant layout will be namespaced
    under the tag. While implementing this, cleaned up the layout directory
    creation by making it a separate function which needs to be called
    explicitly, thus `new` is now side-effect free (which is good!). Also
    added a corresponding `delete_fs_path` function. Added a flag to test
    whether the layout exists on the FS.

--- src/image/oci/layout.rs
+++ src/image/oci/layout.rs
@@ -1,6 +1,21 @@
 //! Handling related to OCI Image Layout as defined here
 //! https://github.com/opencontainers/image-spec/blob/master/image-layout.md
 
+//! # Notes
+//!
+//! A container image will be stored on a file system using the layout as discussed in the
+//! document above. For our use cases, we'll be storing an image inside a directory on a file
+//! system under a path that looks like `<BASE_DIR>/<name>/[<tag>]/`. This allows us to store
+//! images separately for individual tags, each with their own 'layout'. The `tag` is optional,
+//! when creating a local image layout, when images are copied from a Docker reference say, if the
+//! tag is implicit (like `latest`), it will be used. In general this should address all the use
+//! cases. One particular issue is - what if a user tries to write to a layout that already exists.
+//! This is not extremely uncommon (though not very common as well.) Let's say there already exists
+//! an image in the FS (downloaded as part of `copy` or `pull`) and next action is going to
+//! overwrite this. The best is to simply warn and provide a force option to overwrite, deleting
+//! existing (`index.json` and perhaps some `blobs` as well.)
+//!
+
 use std::error::Error as StdError;
 use std::fmt::Display;
 use std::path::PathBuf;
@@ -15,6 +30,7 @@ use crate::utils::oci_images_root;
 
 const OCI_LAYOUT_FILENAME: &str = "oci-layout";
 const INDEX_JSON_FILENAME: &str = "index.json";
+const BLOBS_DIRNAME: &str = "blobs";
 
 #[derive(Debug)]
 pub struct OCIImageLayoutError(String);
@@ -41,27 +57,58 @@ impl From<serde_json::Error> for OCIImageLayoutError {
 
 pub(crate) struct OCIImageLayout {
     pub(crate) name: String,
+    pub(crate) tag: Option<String>,
     pub(crate) image_path: PathBuf,
     pub(crate) index: Index,
     pub(crate) layout: ImageLayout,
+    pub(crate) fs_path_exists: bool,
 }
 
 impl OCIImageLayout {
-    /// `OCIImageLayout` structure from the image name.
-    pub fn new(name: &str) -> Result<Self, OCIImageLayoutError> {
+    /// `OCIImageLayout` structure from the image name and optional tag.
+    pub fn new(name: &str, tag: Option<&str>) -> Self {
         // It's okay to 'panic' if we can't get the base path.
         let mut image_path =
             oci_images_root().expect("Unable to get Base Directory for OCI Images.");
-        let _ = image_path.push(name);
+        if tag.is_none() {
+            let _ = image_path.push(name);
+        } else {
+            let _ = image_path.push(format!("{}/{}", name, tag.unwrap()));
+        }
 
-        let _ = std::fs::create_dir_all(&image_path)?;
+        let tag = match tag {
+            Some(t) => Some(t.to_string()),
+            None => None,
+        };
 
-        Ok(OCIImageLayout {
+        OCIImageLayout {
             name: name.to_string(),
+            tag,
             image_path,
             index: Index::default(),
             layout: ImageLayout::default(),
-        })
+            fs_path_exists: false,
+        }
+    }
+
+    /// Create the Layout on the FS
+    ///
+    /// Creates the underlying 'blobs' directory as well (As it is a required one.)
+    pub async fn create_fs_path(&mut self) -> Result<(), OCIImageLayoutError> {
+        let mut path = self.image_path.clone();
+        path.push(BLOBS_DIRNAME);
+        let _ = tokio::fs::create_dir_all(&path).await?;
+        self.fs_path_exists = true;
+
+        Ok(())
+    }
+
+    /// Delete the Layout from the FS
+    pub async fn delete_fs_path(&mut self) -> Result<(), OCIImageLayoutError> {
+        let _ = tokio::fs::remove_dir_all(&self.image_path).await?;
+        self.fs_path_exists = false;
+
+        Ok(())
     }
 
     /// Write Image Layout file.
@@ -112,14 +159,18 @@ mod tests {
 
     #[tokio::test]
     async fn test_basic_layout() {
-        let oci_layout = OCIImageLayout::new("foo");
-        assert!(oci_layout.is_ok());
-        let oci_layout = oci_layout.unwrap();
+        let mut oci_layout = OCIImageLayout::new("foo", None);
+
+        let r = oci_layout.create_fs_path().await;
+        assert!(r.is_ok());
 
         let r = oci_layout.write_index_json().await;
         assert!(r.is_ok(), "{:#?}", r.err());
 
         let r = oci_layout.write_image_layout().await;
         assert!(r.is_ok(), "{:#?}", r.err());
+
+        let r = oci_layout.delete_fs_path().await;
+        assert!(r.is_ok());
     }
 }
