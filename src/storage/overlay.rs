f66d0d75c466b7092c62f7f861e5eeab08437178
Abhijit Gadgil <gabhijit@iitbombay.org>
2021-02-23 16:58:03 +0530

    Continuing implementation of `apply_layer`
    This implementation right now uses `sync` API, at some point we should
    be moving to `async` API.

--- src/storage/overlay.rs
+++ src/storage/overlay.rs
@@ -1,6 +1,7 @@
 //! Functionality related to handling 'overlay' file-system
 
-use std::path::PathBuf;
+use std::io::BufReader;
+use std::path::{Path, PathBuf};
 
 use crate::{image::oci::digest::Digest, utils::storage_root_for_fs};
 
@@ -18,13 +19,59 @@ pub fn layers_base_path() -> std::io::Result<PathBuf> {
 ///
 /// For the 'overlay' filesystem, this involves, extracting the tar files and handling the
 /// whiteouts.
-pub fn apply_layer(digest: &Digest, layer: &PathBuf) -> std::io::Result<()> {
-    let mut layer_path = layers_base_path()?;
+pub fn apply_layer<P: AsRef<Path>>(
+    digest: &Digest,
+    layer: P,
+    base_path: Option<&PathBuf>,
+) -> std::io::Result<()> {
+    let mut layer_path: PathBuf;
+    if base_path.is_none() {
+        layer_path = layers_base_path()?;
+    } else {
+        layer_path = PathBuf::from(base_path.unwrap())
+    }
 
     let _ = layer_path.push(format!("{}/{}", digest.algorithm(), digest.hex_digest()));
 
     if !layer_path.exists() {
         std::fs::create_dir_all(layer_path)?;
     }
+
+    let reader = BufReader::new(std::fs::File::open(layer)?);
+    let gz_reader = flate2::bufread::GzDecoder::new(reader);
+    let mut tar_reader = tar::Archive::new(gz_reader);
+
+    let entries = tar_reader.entries()?;
+
+    for entry in entries {
+        println!("{:#?}", entry?.header());
+    }
+
     Ok(())
+}
+
+// TODO: Implement an API - cleanup layer
+// Given a layer digest, cleans up the given layer. deletes everything under the directory for the
+// layer.
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::image::oci::digest::Digest;
+
+    // FIXME: Hard coded
+    #[test]
+    fn test_apply_layer() {
+        assert!(true);
+        let digest = Digest::new_from_str(
+            "sha256:5c4213be9af904dd74649d250f22023f532b2f9179ffcb15260b5eaa10d7a3b4",
+        )
+        .unwrap();
+        let blobpath =
+            "/home/gabhijit/.local/share/intmod/images/docker.io/library/busybox/1.32/blobs/sha256/5c4213be9af904dd74649d250f22023f532b2f9179ffcb15260b5eaa10d7a3b4";
+
+        let r = apply_layer(&digest, blobpath, Some(&PathBuf::from("/tmp")));
+        assert!(r.is_ok());
+    }
 }
