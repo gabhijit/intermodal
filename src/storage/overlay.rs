da26829d9e23cca5863a7eb0d93843b3ae73c222
Abhijit Gadgil <gabhijit@iitbombay.org>
2021-02-25 17:07:40 +0530

    First cut implementation of `apply_layer`
    Handles white out entries as well.

--- src/storage/overlay.rs
+++ src/storage/overlay.rs
@@ -1,9 +1,16 @@
 //! Functionality related to handling 'overlay' file-system
 
-use std::io::BufReader;
+use std::ffi::CString;
+use std::io::{BufReader, Write};
 use std::path::{Path, PathBuf};
 
 use crate::{image::oci::digest::Digest, utils::storage_root_for_fs};
+
+// Constants specific to overlay FS
+const WHITEOUT_PREFIX: &str = ".wh.";
+const WHITEOUT_OPAQUE: &str = ".wh..wh..opq";
+const XATTR_OVERLAY_FS_OPAQUE_KEY: &str = "trusted.overlay.opaque";
+const XATTR_OVERLAY_FS_OPAQUE_VAL: &[u8; 1] = b"y";
 
 /// Returns the Path to the 'layers' directory.
 pub fn layers_base_path() -> std::io::Result<PathBuf> {
@@ -23,6 +30,7 @@ pub fn apply_layer<P: AsRef<Path>>(
     digest: &Digest,
     layer: P,
     base_path: Option<&PathBuf>,
+    lower: &str,
 ) -> std::io::Result<()> {
     let mut layer_path: PathBuf;
     if base_path.is_none() {
@@ -33,8 +41,29 @@ pub fn apply_layer<P: AsRef<Path>>(
 
     let _ = layer_path.push(format!("{}/{}", digest.algorithm(), digest.hex_digest()));
 
+    // Create the directory identified by the checksum
     if !layer_path.exists() {
-        std::fs::create_dir_all(layer_path)?;
+        std::fs::create_dir_all(&layer_path)?;
+    }
+
+    // create the 'diff' directory - This is where 'rootFS' will be mounted
+    let mut diff_path = PathBuf::from(&layer_path);
+    let _ = diff_path.push("diff");
+    std::fs::create_dir_all(&diff_path)?;
+
+    // Writes the 'lower' file as per docker's overlay2
+    //
+    // Also creates 'work' dir required by 'overalyfs'
+    if !lower.is_empty() {
+        let mut lower_path = PathBuf::from(&layer_path);
+        let _ = lower_path.push("lower");
+        let mut f = std::fs::File::create(lower_path)?;
+        f.write_all(lower.as_bytes())?;
+        f.sync_all()?;
+
+        let mut workdir_path = PathBuf::from(&layer_path);
+        let _ = workdir_path.push("work");
+        std::fs::create_dir(workdir_path)?;
     }
 
     let reader = BufReader::new(std::fs::File::open(layer)?);
@@ -44,9 +73,63 @@ pub fn apply_layer<P: AsRef<Path>>(
     let entries = tar_reader.entries()?;
 
     for entry in entries {
-        println!("{:#?}", entry?.header());
+        let mut entry = entry?;
+        let is_whiteout = entry
+            .path()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .contains(WHITEOUT_PREFIX);
+        if is_whiteout {
+            // Handle whiteout will do everything to
+            // 1. 'write' the entry to the FS if required
+            // 2. 'create' char(0, 0) device at the path.
+            // 3. set `xattr` etc.
+            handle_whiteout(&diff_path, &entry)?;
+        } else {
+            // Not a white-out simply write the entry to the path.
+            let _ = entry.unpack_in(&diff_path)?;
+        }
+        println!("{:#?}", entry.header());
     }
 
+    Ok(())
+}
+
+// Handles the whiteout entry for the Overlay FS
+//
+// Ref: https://www.kernel.org/doc/html/latest/filesystems/overlayfs.html
+//
+fn handle_whiteout<'a, P, R>(base: P, entry: &tar::Entry<'a, R>) -> std::io::Result<()>
+where
+    P: AsRef<Path>,
+    R: 'a + std::io::Read,
+{
+    // An Opaque whiteout entry.
+    let entry_path = entry.path().unwrap();
+    if entry_path.ends_with(WHITEOUT_OPAQUE) {
+        let mut components = entry_path.components();
+        if let Some(_) = components.next_back() {
+            // Last is consumed. use whatever remains as a path.
+            let joined = base.as_ref().join(components.as_path());
+            std::fs::create_dir_all(&joined)?;
+            xattr::set(
+                joined,
+                XATTR_OVERLAY_FS_OPAQUE_KEY,
+                XATTR_OVERLAY_FS_OPAQUE_VAL,
+            )?;
+        }
+    } else {
+        let mknod_path_str = entry_path.to_str().unwrap().replace(WHITEOUT_PREFIX, "");
+        let mknod_path = Path::new(&mknod_path_str);
+        let joined_path = base.as_ref().join(mknod_path);
+        let joined_str = joined_path.to_str().unwrap();
+        let joined_cstr = CString::new(joined_str)?;
+
+        unsafe {
+            libc::mknod(joined_cstr.as_ptr(), libc::S_IFCHR, libc::makedev(0, 0));
+        }
+    }
     Ok(())
 }
 
@@ -98,6 +181,7 @@ mod tests {
             &digest,
             blobpath,
             Some(&PathBuf::from(layout_tempdir.path())),
+            "",
         );
         assert!(r.is_ok());
     }
